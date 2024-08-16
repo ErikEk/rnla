62897d032372e6534c8c1af0e19b8c6611126bbb
ErikEk <erik.ek@gmail.com>
2024-08-16 13:45:54 +0700

    s

--- svd.py
+++ svd.py
@@ -6,3 +6,14 @@ import pandas as pd
 import scipy.stats
 import sklearn.datasets
 import sklearn.preprocessing
+
+
+iris = sklearn.datasets.load_iris()
+
+df_iris = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+print('Iris dataset has {} rows and {} columns\n'.format(*df_iris.shape))
+
+print('Here are the first 5 rows of the data:\n\n{}\n'.format(df_iris.head(5)))
+
+print('Some simple statistics on the Iris dataset:\n\n{}\n'.format(df_iris.describe()))

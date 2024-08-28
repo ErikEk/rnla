89186298a9304a931ffb7e6246656dab85be88ac
ErikEk <erik.ek@gmail.com>
2024-08-28 06:34:12 +0200

    s

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ X = iris.data #np.random.normal(loc=0.0, scale=1.0, size=(100,50))
 M, N = X.shape
 K = 10
 Omega = np.random.normal(loc=0.0, scale=1.0, size=(N,K))
-#print(X)
-#Y = X * Omega
+
 Y = np.dot(X, Omega)
 print(Y.shape)
 Q,R = np.linalg.qr(Y,mode='reduced')
@@ -29,7 +28,11 @@ print("ss")
 
 B = np.transpose(Q).dot(X)
 
-U,S,V = np.linalg.svd(B)
-print(U.shape)
-uapp = Q.dot(U)
-print(uapp.shape)
+U_hat,sum_hat,v_hat = np.linalg.svd(B)
+print(U_hat.shape)
+U = Q.dot(U_hat)
+
+print('matrix U has {} rows, {} columns\n'.format(*U.shape))
+print('here are the first 5 rows.')
+
+print('{}'.format(pd.DataFrame(U).head(5)))
